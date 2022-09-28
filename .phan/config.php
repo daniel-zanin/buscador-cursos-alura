baaaa94b9dfa7bbef19cf18ba32eb4b0f12c6a4d
Daniel M. Zanin <dmzanin@ecossistemaanima.com.br>
2022-09-28 12:27:56 -0300

    Iniciando projeto

---
+++ .phan/config.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This configuration will be read and overlaid on top of the
+ * default configuration. Command line arguments will be applied
+ * after this file is read.
+ * 
+ * composer require --dev phan/phan
+ */
+return [
+
+    "target_php_version" => '7.4',
+
+    'directory_list' => [
+        'src',
+        'vendor/symfony/dom-crawler',
+        'vendor/guzzlehttp/guzzle',
+        'vendor/psr/http-message'
+    ],
+
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+    
+    'plugins' => [
+        // checks if a function, closure or method unconditionally returns.
+        // can also be written as 'vendor/phan/phan/.phan/plugins/AlwaysReturnPlugin.php'
+        'AlwaysReturnPlugin',
+        'DollarDollarPlugin',
+        'DuplicateArrayKeyPlugin',
+        'DuplicateExpressionPlugin',
+        'PregRegexCheckerPlugin',
+        'PrintfCheckerPlugin',
+        'SleepCheckerPlugin',
+        // Checks for syntactically unreachable statements in
+        // the global scope or function bodies.
+        'UnreachableCodePlugin',
+        'UseReturnValuePlugin',
+        'EmptyStatementListPlugin',
+        'LoopVariableReusePlugin',
+    ],
+];
---
+++ README.md
@@ -0,0 +1,3 @@
+# Buscador de Cursos ALURA
+
+Lista os cursos PHP disponíveis na plataforma de ensino Alura.
